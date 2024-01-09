5fdbf40ccd449c4b3e088f7c165e37e7d7b66bd2
mohdwarish <khanwarish483@gmail.com>
2024-01-10 02:35:28 +0530

    added landing page

--- learnunit-frontend/src/components/Layout/Theme.js
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  colors: {
-    customBlue: "black", // Replace with your desired blue color code
-  },
-});
-
-export default theme;
